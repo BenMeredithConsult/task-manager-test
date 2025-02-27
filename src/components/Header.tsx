a2d94148d8ffbc283c1f3c146c10844146bc20e1
obintou <bintouongoiba2018@gmail.com>
2025-02-27 11:02:47 +0000

    test gestionnaire de tache par Bintou

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header className="text-start p-6 bg-amber-500">
+        <h1 className="text-2xl font-bold">Gestion des tâches</h1>
+      </header>
+    );
+  };
+  export default Header;
+  
